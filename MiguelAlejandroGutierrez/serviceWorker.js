2873d80701386169e992446c27ce98b0a56e12bf
MiguelAlejandroGO <78164320+MiguelAlejandroGO@users.noreply.github.com>
2021-10-18 12:52:14 -0500

    Practica 2

--- MiguelAlejandroGutierrez/serviceWorker.js
+++ MiguelAlejandroGutierrez/serviceWorker.js
@@ -21,30 +21,46 @@ self.addEventListener("install", installEvent => {
 
   installEvent.waitUntil(_openCache());
 });
+/*1. Cache Only 
 
-/*1. Cache Only */
+self.addEventListener('fetch', event => {
+  //verifica si la peticion de la peticion este en el cache
+  event.respondWith(caches.match(event.request));
+});*/
 
-// self.addEventListener('fetch', event => {
-//   //verifica si la peticion de la peticion este en el cache
-//   event.respondWith(caches.match(event.request));
-// });
-
- 
-// 2. Network Only
+/* 2. Network Only
 self.addEventListener('fetch', event => { 
   event.respondWith(fetch(event.request));
-});
+});*/
 
+// 3. Cache First
+self.addEventListener("fetch", fetchEvent => {
+  /*Cache First
+    const response = caches.match(fetchEvent.request).then( res => {
+      console.log(res);
+    })
+    .catch( res => {
+      console.log('No existe el request ' + fetchEvent.request);
+      console.log(res);
+    });*/
 
+    fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then(res => {
+        return res || fetch(fetchEvent.request);
+      })
+    );
+    
+   });
 
-
-
-// self.addEventListener("fetch", fetchEvent => {
-//     fetchEvent.respondWith(
-//       caches.match(fetchEvent.request).then(res => {
-//         return res || fetch(fetchEvent.request);
-//       })
-//     )
-//   });
+   // 3. Network First
+self.addEventListener("fetch", fetchEvent => {
+  
+    fetchEvent.respondWith(
+      fetch(fetchEvent.request).then(res => {
+        return   fetch(fetchEvent.request || res);
+      })
+    );
+    
+   });
 
 
